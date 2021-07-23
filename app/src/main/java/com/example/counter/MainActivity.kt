af06f4bf4a8dc0d729f5dcbf6a8012862cc36a59
kris23lis <kris23lis@gmail.com>
2021-07-23 17:43:43 +0300

    Создала счетчик, с сохранением переворота и доп экраном

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -10,13 +10,52 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    lateinit var countTV : TextView
+    lateinit var btn : Button
+    var count = 0
+
+    companion object{
+        const val COUNT_MESSAGE = "com.example.counter.COUNTER"
+        const val PUT_KEY = "COUNT"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        initView()
+        if (savedInstanceState != null){
+            countTV.text = savedInstanceState.getString(PUT_KEY)
+            count = savedInstanceState.getInt(PUT_KEY)
+            countTV.text = count.toString()
+        }
+        btnClick(btn)
+        textViewClick(countTV)
     }
 
+    private fun initView(){
+        countTV = findViewById(R.id.count_textView)
+        btn = findViewById(R.id.button)
+    }
 
+    private fun btnClick(btn: Button){
+        btn.setOnClickListener {
+            count++
+            if (countTV!=null)
+                countTV.text = count.toString()
+        }
+    }
 
+    private fun textViewClick(textView: TextView){
+        textView.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra(COUNT_MESSAGE, count.toString())
+            startActivity(intent)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState, )
+        outState.putString(PUT_KEY, countTV.text.toString())
+        outState.putInt(PUT_KEY, count)
+    }
 }
