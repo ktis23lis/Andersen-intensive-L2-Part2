fa8abde49cbba722101cd49e1c285939557462b1
kris23lis <kris23lis@gmail.com>
2021-07-23 17:38:46 +0300

    init repository

---
+++ app/src/main/java/com/example/counter/DetailActivity.kt
@@ -0,0 +1,9 @@
+package com.example.counter
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.counter
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+
+}
